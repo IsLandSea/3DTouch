8125ff346cd068c4b4d640a92f7b900d7415120c
IsLand <lihaidao@baaaaby.com>
2016-01-19 14:45:53 +0800

    修改杀死aop 3D Touch 不能跳转bug

--- 3D Touch/AppDelegate.m
+++ 3D Touch/AppDelegate.m
@@ -11,6 +11,7 @@
 @interface AppDelegate ()
 
 @property (nonatomic,strong) ViewController *vc;
+@property (nonatomic,strong) UIApplicationShortcutItem *currentItem;
 
 @end
 
@@ -18,8 +19,6 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    
-    
     
     /*
      当程序启动时
@@ -30,55 +29,50 @@
      */
     
     [self creatUIApplicationShortcutItems];
-
+    
     if (launchOptions[@"UIApplicationLaunchOptionsShortcutItemKey"] == nil) {
         
         return YES;
         
     } else {
         
+        _currentItem = launchOptions[UIApplicationLaunchOptionsShortcutItemKey];
+        
         return NO;
     }
-
+    
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
+    if (_currentItem) {
+        [self handleShortcartItem:_currentItem];
+        _currentItem = nil;
+    }
+    
 }
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-}
-
 /** 处理shortcutItem */
 - (void)application:(UIApplication *)application performActionForShortcutItem:(UIApplicationShortcutItem *)shortcutItem completionHandler:(void (^)(BOOL))completionHandler {
     
+    [self handleShortcartItem:shortcutItem];
+    
+}
+- (void)handleShortcartItem:(UIApplicationShortcutItem *)shortcutItem {
+    
+    
     if ([shortcutItem.type isEqualToString:@"first"])
     {
-       
+        
         NSLog(@"doSomeThing");
         
     }else if ([shortcutItem.type isEqualToString:@"second"]) {
-    
+        
         NSLog(@"doSomething");
-    
+        
     }
+    
 }
-
 -(void)creatUIApplicationShortcutItems
 {
     UIApplicationShortcutIcon *icon1 = [UIApplicationShortcutIcon iconWithTemplateImageName:@"gouwuche"];
@@ -87,7 +81,7 @@
     
     
     //创建快捷item的userinfo 即UIApplicationShortcutItemUserInfo
-   
+    
     NSDictionary *info1 = @{@"url":@"money"};
     
     //创建ShortcutItem
